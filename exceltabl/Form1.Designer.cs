1f3bd6b3c24d47d5997378ddee860bbd5736a02a
Damirdb <115777005+Damirdb@users.noreply.github.com>
2025-05-28 12:03:47 +0500

    Form1.Designer.cs

--- exceltabl/Form1.Designer.cs
+++ exceltabl/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace exceltabl
+namespace exceltabl
 {
     partial class Form1
     {
@@ -22,22 +22,26 @@
             this.lblFilePath = new System.Windows.Forms.Label();
             this.progressBar = new System.Windows.Forms.ProgressBar();
             this.lblStatus = new System.Windows.Forms.Label();
+            this.button_max = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvResult)).BeginInit();
             this.SuspendLayout();
             // 
             // dgvResult
             // 
             this.dgvResult.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvResult.Location = new System.Drawing.Point(12, 12);
+            this.dgvResult.Location = new System.Drawing.Point(16, 15);
+            this.dgvResult.Margin = new System.Windows.Forms.Padding(4);
             this.dgvResult.Name = "dgvResult";
-            this.dgvResult.Size = new System.Drawing.Size(760, 400);
+            this.dgvResult.RowHeadersWidth = 51;
+            this.dgvResult.Size = new System.Drawing.Size(1013, 492);
             this.dgvResult.TabIndex = 0;
             // 
             // btnOpenFile
             // 
-            this.btnOpenFile.Location = new System.Drawing.Point(12, 418);
+            this.btnOpenFile.Location = new System.Drawing.Point(16, 514);
+            this.btnOpenFile.Margin = new System.Windows.Forms.Padding(4);
             this.btnOpenFile.Name = "btnOpenFile";
-            this.btnOpenFile.Size = new System.Drawing.Size(100, 23);
+            this.btnOpenFile.Size = new System.Drawing.Size(133, 28);
             this.btnOpenFile.TabIndex = 1;
             this.btnOpenFile.Text = "Открыть файл";
             this.btnOpenFile.UseVisualStyleBackColor = true;
@@ -45,9 +49,10 @@
             // 
             // btnRun
             // 
-            this.btnRun.Location = new System.Drawing.Point(118, 418);
+            this.btnRun.Location = new System.Drawing.Point(157, 514);
+            this.btnRun.Margin = new System.Windows.Forms.Padding(4);
             this.btnRun.Name = "btnRun";
-            this.btnRun.Size = new System.Drawing.Size(100, 23);
+            this.btnRun.Size = new System.Drawing.Size(133, 28);
             this.btnRun.TabIndex = 2;
             this.btnRun.Text = "Оптимизировать";
             this.btnRun.UseVisualStyleBackColor = true;
@@ -55,9 +60,10 @@
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(672, 418);
+            this.btnSave.Location = new System.Drawing.Point(896, 514);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(4);
             this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(100, 23);
+            this.btnSave.Size = new System.Drawing.Size(133, 28);
             this.btnSave.TabIndex = 3;
             this.btnSave.Text = "Сохранить";
             this.btnSave.UseVisualStyleBackColor = true;
@@ -66,32 +72,46 @@
             // lblFilePath
             // 
             this.lblFilePath.AutoSize = true;
-            this.lblFilePath.Location = new System.Drawing.Point(12, 444);
+            this.lblFilePath.Location = new System.Drawing.Point(16, 546);
+            this.lblFilePath.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblFilePath.Name = "lblFilePath";
-            this.lblFilePath.Size = new System.Drawing.Size(0, 13);
+            this.lblFilePath.Size = new System.Drawing.Size(0, 16);
             this.lblFilePath.TabIndex = 4;
             // 
             // progressBar
             // 
-            this.progressBar.Location = new System.Drawing.Point(224, 418);
+            this.progressBar.Location = new System.Drawing.Point(451, 514);
+            this.progressBar.Margin = new System.Windows.Forms.Padding(4);
             this.progressBar.Name = "progressBar";
-            this.progressBar.Size = new System.Drawing.Size(200, 23);
+            this.progressBar.Size = new System.Drawing.Size(114, 28);
             this.progressBar.TabIndex = 5;
             this.progressBar.Visible = false;
             // 
             // lblStatus
             // 
             this.lblStatus.AutoSize = true;
-            this.lblStatus.Location = new System.Drawing.Point(430, 423);
+            this.lblStatus.Location = new System.Drawing.Point(573, 521);
+            this.lblStatus.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblStatus.Name = "lblStatus";
-            this.lblStatus.Size = new System.Drawing.Size(0, 13);
+            this.lblStatus.Size = new System.Drawing.Size(0, 16);
             this.lblStatus.TabIndex = 6;
+            // 
+            // button_max
+            // 
+            this.button_max.Location = new System.Drawing.Point(297, 514);
+            this.button_max.Name = "button_max";
+            this.button_max.Size = new System.Drawing.Size(143, 28);
+            this.button_max.TabIndex = 7;
+            this.button_max.Text = "Максимальное";
+            this.button_max.UseVisualStyleBackColor = true;
+            this.button_max.Click += new System.EventHandler(this.button_max_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.ClientSize = new System.Drawing.Size(1045, 567);
+            this.Controls.Add(this.button_max);
             this.Controls.Add(this.lblStatus);
             this.Controls.Add(this.progressBar);
             this.Controls.Add(this.lblFilePath);
@@ -99,8 +119,10 @@
             this.Controls.Add(this.btnRun);
             this.Controls.Add(this.btnOpenFile);
             this.Controls.Add(this.dgvResult);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Оптимизатор учебной нагрузки";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvResult)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -114,5 +136,6 @@
         private System.Windows.Forms.Label lblFilePath;
         private System.Windows.Forms.ProgressBar progressBar;
         private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button button_max;
     }
 }
